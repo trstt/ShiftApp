0d80139492a0ce2bbcda6290fac1ac7952d8d53f
Torsti P <Torsti P>
2017-05-05 14:06:44 +0300

    fixes and removals

--- www/js/time.js
+++ www/js/time.js
@@ -7,9 +7,9 @@
 		quarters: Math.floor(d.getMinutes() / 15)
 	};
 
-if(nowDate.year === 2017 /*&& nowDate.month === 4 && nowDate.day === 7*/){
+if(nowDate.year === 2017 && nowDate.month === 5 && nowDate.day === 31){
     $(".verticalLine").css("display", "inline");
-    var left = (nowDate.hours - 10) * 4*30 + nowDate.quarters*30+14;
+    var left = (nowDate.hours - 10) * 4*30 + nowDate.quarters*30+17.3;
     $(".verticalLine").css("left", left+"vw");
     var vwscroll = window.innerWidth/100 * left - window.innerWidth/2;
     $(".schedule").scrollLeft(vwscroll);
